9a314cea746c0e00f3d8a65fe2328a0ed97d59c6
Abumahfouz <mahfoozabubakar@rocketmail.com>
2026-02-10 17:22:02 +0100

    updated delete controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -25,6 +25,67 @@ const createPost = async(req, res) => {
     }
 }
 
+// @desc  get all posts
+// @route GET /api/posts
+// @access Public
+const getPosts = async(req, res) => {
+    try {
+        const posts = await Post.find().populate('author', 'username', );
+        res.status(200).json(posts);
+    }
+    catch (error) {
+        return res.status(500).json({ message: "Error fetching posts", error });
+    }
+}
+
+
+// @desc  update a post
+// @route PUT /api/posts/:id
+// @access Private (only the author or admin can update)
+const updatePost = async(req, res) => {
+    const { id } = req.params;
+    const { title, content, tags } = req.body;
+    const userID = req.user._id;
+    try {
+        const post = await Post.findById(id);
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+        if (post.author.toString() !== userID && req.user.role !== 'admin') {
+            return res.status(403).json({ message: "Unauthorized to update this post" });
+        }
+        const updatedPost = await Post.findByIdAndUpdate(id, { title, content, tags }, { new: true });
+        res.status(200).json(updatedPost);
+    } catch (error) {
+        return res.status(500).json({ message: "Error updating post", error });
+    }
+}
+
+// @desc delete a post
+// @route DELETE /api/posts/:id
+// @access Private (only the author or admin can delete)
+
+const deletePost = async(req, res) => {
+    const { id } = req.params;
+    const userID = req.user._id;
+    try {
+        const post = await Post.findById(id);
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+        if (post.author.toString() !== userID && req.user.role !== 'admin') {
+            return res.status(403).json({ message: "Unauthorized to delete this post" });
+        }
+        await Post.findByIdAndDelete(id);
+        res.status(200).json({ message: "Post deleted successfully" });
+    } catch (error) {
+        return res.status(500).json({ message: "Error deleting post", error });
+    }
+}
+
 module.exports = {
     createPost,
+    getPosts,
+    updatePost,
+    deletePost
 }
